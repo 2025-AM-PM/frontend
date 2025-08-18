bfce32926ce2e31554787d0a1a8103e367b372b6
HanJunSeoJB <hjs123@jbnu.ac.kr>
2025-08-18 14:27:09 +0900

    fix: 종강 시계 디자인 수정

--- am-pm/src/pages/Home.tsx
+++ am-pm/src/pages/Home.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import Header from "../components/header";
 import "../styles/home.css";
 import Board from "../components/board";
-import { Clock } from "../components/clock";
+import Clock from "../components/clock";
 import Background from "../components/background";
 
 function HomePage() {
   return (
     <div className="main">
       <Header />
-      <Clock initialSeconds={3186254} />
+      <Clock />
       <Background />
       <Board />
     </div>
