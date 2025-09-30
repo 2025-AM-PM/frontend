31bfa7c384aa697ce7e4861c935e9d951a7070ab
HanJunseoJB <hjs123@jbnu.ac.kr>
2025-09-30 16:38:46 +0900

    feat: 게시글 드롭다운 메뉴 추가

--- am-pm/src/components/header.tsx
+++ am-pm/src/components/header.tsx
@@ -1,5 +1,5 @@
 import "../styles/header.css";
-import { Routes, Route, NavLink, useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import UserInfo from "./User";
 import { User } from "../types";
 import LoginBtn from "./loginBtn";
@@ -26,70 +26,133 @@ function Header() {
       window.removeEventListener("storage", sync);
       window.removeEventListener("focus", sync);
     };
-    // location.pathname이 바뀔 때마다 재동기화
   }, [location.pathname]);
+
+  // 현재 경로 매칭 유틸 (exact 또는 하위 경로 포함)
+  const isCurrentPath = (base: string, exact = false) => {
+    const path = location.pathname;
+    if (exact) return path === base;
+    return path === base || path.startsWith(base + "/");
+  };
 
   return (
     <header className="header">
       <div className="container">
         {/* 로고 */}
-        <div className="logo"></div>
+        <div className="logo" />
 
-        {/* 네비바 */}
-        <nav className="nav">
-          <ul className="nav-list">
+        {/* 네비바 (BEM 구조) */}
+        <nav className="nav" aria-label="Primary">
+          <ul className="nav__list">
             <li
-              className={`nav-item${
-                location.pathname === "/" ? " nav-item-current" : ""
-              }`}
+              className={
+                "nav__item" +
+                (isCurrentPath("/", true) ? " nav__item--current" : "")
+              }
             >
-              <NavLink to="/" end>
+              <NavLink to="/" end className="nav__link">
                 홈
               </NavLink>
             </li>
+
             <li
-              className={`nav-item${
-                location.pathname === "/projects" ? " nav-item-current" : ""
-              }`}
+              className={
+                "nav__item" +
+                (isCurrentPath("/projects") ? " nav__item--current" : "")
+              }
             >
-              <NavLink to="/projects">프로젝트 자랑</NavLink>
-            </li>
-            <li
-              className={`nav-item${
-                location.pathname === "/rank" ? " nav-item-current" : ""
-              }`}
-            >
-              <NavLink to="/rank">백준 랭크</NavLink>
-            </li>
-            <li
-              className={`nav-item${
-                location.pathname === "/polls" ? " nav-item-current" : ""
-              }`}
-            >
-              <NavLink to="/polls">투표</NavLink>
+              <NavLink to="/projects" className="nav__link">
+                프로젝트 자랑
+              </NavLink>
             </li>
 
             <li
-              className={`nav-item${
-                location.pathname === "/polls" ? " nav-item-current" : ""
-              }`}
+              className={
+                "nav__item" +
+                (isCurrentPath("/rank") ? " nav__item--current" : "")
+              }
             >
-              <NavLink to="/boards">게시판</NavLink>
-              <ul>
-                <li>스터디</li>
-                <li>공지사항</li>
-                <li>취업정보</li>
-                <li>학교정보</li>
+              <NavLink to="/rank" className="nav__link">
+                백준 랭크
+              </NavLink>
+            </li>
+
+            <li
+              className={
+                "nav__item" +
+                (isCurrentPath("/polls") ? " nav__item--current" : "")
+              }
+            >
+              <NavLink to="/polls" className="nav__link">
+                투표
+              </NavLink>
+            </li>
+
+            {/* 드롭다운: 게시판 */}
+            <li
+              className={
+                "nav__item nav__item--has-submenu" +
+                (isCurrentPath("/boards") ? " nav__item--current" : "")
+              }
+            >
+              <NavLink
+                to="/boards"
+                className="nav__link"
+                aria-haspopup="true"
+                aria-expanded={isCurrentPath("/boards") ? true : undefined}
+              >
+                게시판
+              </NavLink>
+              <ul className="nav__submenu" role="menu">
+                <li className="nav__submenu-item" role="none">
+                  <NavLink
+                    to="/boards/study"
+                    className="nav__submenu-link"
+                    role="menuitem"
+                  >
+                    스터디
+                  </NavLink>
+                </li>
+                <li className="nav__submenu-item" role="none">
+                  <NavLink
+                    to="/boards/notice"
+                    className="nav__submenu-link"
+                    role="menuitem"
+                  >
+                    공지사항
+                  </NavLink>
+                </li>
+                <li className="nav__submenu-item" role="none">
+                  <NavLink
+                    to="/boards/jobs"
+                    className="nav__submenu-link"
+                    role="menuitem"
+                  >
+                    취업정보
+                  </NavLink>
+                </li>
+                <li className="nav__submenu-item" role="none">
+                  <NavLink
+                    to="/boards/school"
+                    className="nav__submenu-link"
+                    role="menuitem"
+                  >
+                    학교정보
+                  </NavLink>
+                </li>
               </ul>
             </li>
 
             {user && user.role && user.role !== "USER" && (
               <li
-                className={`nav-item${
-                  location.pathname === "/admin" ? " nav-item-current" : ""
-                }`}
+                className={
+                  "nav__item" +
+                  (isCurrentPath("/admin") ? " nav__item--current" : "")
+                }
               >
-                <NavLink to="/admin">관리자</NavLink>
+                <NavLink to="/admin" className="nav__link">
+                  관리자
+                </NavLink>
               </li>
             )}
           </ul>
